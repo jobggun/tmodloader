a82156e0e57849d065995e49621c169d787bec8c
Jobggun <48022705+jobggun@users.noreply.github.com>
2023-03-09 09:55:57 +0900

    unpriviliged user

--- tmodloader/bootstrap.sh
+++ tmodloader/bootstrap.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 echo "Bootstrap:"
+echo "tmlsavedirectory=$TML_SAVE_DIRECTORY"
 echo "world_file_name=$WORLD_FILENAME"
-echo "logpath=$LOGPATH"
+echo "logpath=/terraria-server/tModLoader-Logs/"
 
-WORLD_PATH="$WORLDPATH/$WORLD_FILENAME"
-if [ ! -f "$CONFIGPATH/$CONFIG_FILENAME" ]; then
+WORLD_PATH="$TML_SAVE_DIRECTORY/Worlds/$WORLD_FILENAME"
+
+if [ ! -f "$TML_SAVE_DIRECTORY/$CONFIG_FILENAME" ]; then
     echo "Server configuration not found, running with default server configuration."
-    echo "Please ensure your desired $CONFIG_FILENAME file is volumed into docker: -v <path_to_config_file>:/$CONFIGPATH"
-    cp ./serverconfig-default.txt $CONFIGPATH/$CONFIG_FILENAME
+    echo "Please ensure your desired $CONFIG_FILENAME file is volumed into docker: -v <path_to_config_file>:/$TML_SAVE_DIRECTORY"
+    cp ./serverconfig-default.txt $TML_SAVE_DIRECTORY/$CONFIG_FILENAME
 fi
 
 if [ -z "$WORLD_FILENAME" ]; then
@@ -18,12 +20,12 @@ if [ -z "$WORLD_FILENAME" ]; then
   else
     echo "Running server with command flags: $@"
   fi
-  exec dotnet tModLoader.dll -server -config "$CONFIGPATH/$CONFIG_FILENAME" -logpath "$LOGPATH" "$@"
+  exec dotnet tModLoader.dll -server -tmlsavedirectory "$TML_SAVE_DIRECTORY" -config "$TML_SAVE_DIRECTORY/$CONFIG_FILENAME" "$@"
 else
   echo "Environment WORLD_FILENAME specified"
   if [ -f "$WORLD_PATH" ]; then
     echo "Loading to world $WORLD_FILENAME..."
-    exec dotnet tModLoader.dll -server -config "$CONFIGPATH/$CONFIG_FILENAME" -logpath "$LOGPATH" -world "$WORLD_PATH" "$@"
+    exec dotnet tModLoader.dll -server -tmlsavedirectory "$TML_SAVE_DIRECTORY" -config "$TML_SAVE_DIRECTORY/$CONFIG_FILENAME" -world "$WORLD_PATH" "$@"
   else
     echo "Unable to locate $WORLD_PATH."
     echo "Please make sure your world file is volumed into docker: -v <path_to_world_file>:$WORLDPATH"
